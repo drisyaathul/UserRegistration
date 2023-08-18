f875c6bfa666f723ca7ff75938f8d2e46467d0ee
drisyaathul <drisyak809@gmail.com>
2023-08-19 00:35:59 +0800

    Added: Firstname starts with caps and has minimum 3 characters

--- src/com/userregistrationproblem/UserRegistration.java
+++ src/com/userregistrationproblem/UserRegistration.java
@@ -1,7 +1,15 @@
 package com.userregistrationproblem;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("--- User Registration Problem ---");
+
+        boolean result1 = Pattern.matches("^[A-Z]{1}[a-z]{2,}","Drisya");
+        if (result1)
+            System.out.println("Valid FirstName");
+        else
+            System.out.println("Invalid FirstName");
     }
 }
