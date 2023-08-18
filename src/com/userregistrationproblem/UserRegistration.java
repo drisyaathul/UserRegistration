4163019298572eef59b8bf5d22be406d7d350879
drisyaathul <drisyak809@gmail.com>
2023-08-19 00:44:24 +0800

    Added: Last Name starts with caps and has minimum 3 characters

--- src/com/userregistrationproblem/UserRegistration.java
+++ src/com/userregistrationproblem/UserRegistration.java
@@ -6,10 +6,16 @@ public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("--- User Registration Problem ---");
 
-        boolean result1 = Pattern.matches("^[A-Z]{1}[a-z]{2,}","Drisya");
+        boolean result1 = Pattern.matches("^[A-Z][a-z]{2,}","Drisya");
         if (result1)
             System.out.println("Valid FirstName");
         else
             System.out.println("Invalid FirstName");
+
+        boolean result2 = Pattern.matches("^[A-Z][a-z]{2,}","Athul");
+        if (result2)
+            System.out.println("Valid LastName");
+        else
+            System.out.println("Invalid LastName");
     }
 }
